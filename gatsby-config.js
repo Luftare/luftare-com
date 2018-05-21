ad590b127df07cf5d5f96f503401d1deb5d83e39
Ilmari Koskinen <ilmari.koskinen@houston-inc.com>
2018-05-21 16:38:27 +0300

    Init commit

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Jauzah!',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi</h1>
+    <p>Not so much here.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
