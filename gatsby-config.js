a408dd340c10c1230b949f0a70e72cf1d8eae623
Ilmari Koskinen <ilmari.koskinen@houston-inc.com>
2018-05-23 09:02:38 +0300

    Add gatsby plugin for styled components

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,5 @@ module.exports = {
   siteMetadata: {
     title: 'Jauzah!',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-styled-components'],
 }
