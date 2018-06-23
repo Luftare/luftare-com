a1929ec8e8412ce294e8b85f63f4298c0cc10563
Ilmari Koskinen <ilmari.koskinen1@gmail.com>
2018-06-23 11:50:30 +0300

    Add music image

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,7 @@ import cloudbunnySrc from '../assets/cloudbunny.png'
 import drum from '../assets/drum.png'
 import sail from '../assets/sail.png'
 import threed from '../assets/threed.png'
+import music from '../assets/music.jpg'
 
 const HeroText = styled.div`
   font-size: 2em;
@@ -20,7 +21,9 @@ const HeroText = styled.div`
 
 const Image = styled.div`
   vertical-align: bottom;
-  display: inline-block;
+  align-items: center;
+  justify-content: center;
+  display: inline-flex;
   width: 100%;
   height: 80vw;
   background-image: url('${props => props.src}');
@@ -28,8 +31,19 @@ const Image = styled.div`
   background-position: center;
   background-repeat: no-repeat;
   ${media.tablet`
-    width: 50%;
+    width: ${props => props.wide ? '100%' : '50%'};
     height: 40vw;
+  `}
+`
+
+const TextBox = styled.span`
+  font-size: 2em;
+  color: ${props => props.theme.black};
+  padding: 16px 24px;
+  background-color: rgba(255, 255, 255, 0.8);
+  ${media.tablet`
+    font-size: 3em;
+    padding: 24px 36px;
   `}
 `
 
@@ -45,7 +59,11 @@ const IndexPage = () => (
       <Image src={threed}/>
       <Image src={sail}/>
       <Image src={drum}/>
+      <Image src={music} wide >
+        <TextBox>sounds</TextBox>
+      </Image>
     </Section>
+
     <Section >
       <MusicBox />
     </Section>
