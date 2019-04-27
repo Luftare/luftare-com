c4b778c3783cb0cb73a8df800f2a3915fdbd8e17
Ilmari Koskinen <ilmari.koskinen1@gmail.com>
2019-04-27 17:21:49 +0300

    Fix broken url

--- src/pages/index.js
+++ src/pages/index.js
@@ -168,7 +168,7 @@ const projects = [
     title: 'Drum machine',
     description:
       'A simple drum sampler. Tap the pads to trigger drum sounds or record a beat on the fly. It features a metronome, undo and changing of tempo.',
-    link: '37.139.31.143/drum/',
+    link: 'http://37.139.31.143/drum/',
     icons: ['react', 'audio'],
     imageSrc: drum,
   },
